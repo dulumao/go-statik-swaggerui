6741f8b3eacb497d230aa4aa424cf704aab3ae05
Markus Troßbach <markus.trossbach@kaufland.com>
2019-02-13 15:18:53 +0100

    # update

---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/gin-gonic/gin"
+	_ "github.com/mtrossbach/go-statik-swaggerui/statik"
+	"github.com/rakyll/statik/fs"
+	"io/ioutil"
+)
+
+type SwaggerUI struct {
+	host        string
+	basePath    string
+	swaggerFile string
+	swaggerData []byte
+}
+
+func NewSwaggerUI(host string, basePath string, swaggerFile string) *SwaggerUI {
+	return &SwaggerUI{
+		host:        host,
+		basePath:    basePath,
+		swaggerFile: swaggerFile,
+	}
+}
+
+func (this *SwaggerUI) SetupGin(r *gin.RouterGroup, urlPrefix string, swaggerUi bool) {
+	r.GET(urlPrefix+"/api-docs", this.getSwaggerFile)
+	if swaggerUi {
+		statikFS, err := fs.New()
+		if err != nil {
+			panic(err)
+		}
+		r.StaticFS("/swaggerui", statikFS)
+	}
+}
+
+func (this *SwaggerUI) prepareSwagger() {
+	byteValue, err := ioutil.ReadFile(this.swaggerFile)
+	if err != nil {
+		panic(err)
+	}
+
+	var data map[string]interface{}
+	err = json.Unmarshal(byteValue, &data)
+	if err != nil {
+		panic(err)
+	}
+
+	data["host"] = this.host
+	data["basePath"] = this.basePath
+
+	newByteValue, err := json.Marshal(data)
+	if err != nil {
+		panic(err)
+	}
+
+	this.swaggerData = newByteValue
+}
+
+func (this *SwaggerUI) getSwaggerFile(c *gin.Context) {
+	c.Data(200, "application/json; charset=utf-8", this.swaggerData)
+}
